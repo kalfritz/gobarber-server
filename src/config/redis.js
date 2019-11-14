26035ed71f8b798e9e7cdc422c5be2444353b8a3
Luiz Elias <luizeliasceem@gmail.com>
2019-11-14 11:15:00 -0300

    add redis.createClient()

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,18 @@
-export default {
+/*export default {
   host: process.env.REDIS_CLOUD_HOST || process.env.REDIS_HOST,
   port: process.env.REDIS_CLOUD_PORT || process.env.REDIS_PORT,
   password: process.env.REDIS_CLOUD_PASS,
   auth_pass: process.env.REDIS_CLOUD_PASS
-};
+};*/
+
+import redis from "redis";
+
+const client = redis.createClient({
+  host: process.env.REDIS_CLOUD_HOST || process.env.REDIS_HOST,
+  port: process.env.REDIS_CLOUD_PORT || process.env.REDIS_PORT
+});
+if (process.env.REDIS_CLOUD_PASS) {
+  client.auth(process.env.REDIS_CLOUD_PASS);
+}
+
+export default client;
