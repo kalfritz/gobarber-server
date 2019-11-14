0dbfce2ee70b0953235bb2604a3e15b1b4c773f7
Luiz Elias <luizeliasceem@gmail.com>
2019-11-14 01:28:57 -0300

    add socket setting redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,5 @@
 export default {
-  host: process.env.REDIS_CLOUD_HOST || process.env.REDIS_HOST,
-  port: process.env.REDIS_CLOUD_PORT || process.env.REDIS_PORT
+  socket: process.env.REDIS_CLOUD_SOCKET,
+  host: process.env.REDIS_CLOUD_HOST ? "" : process.env.REDIS_HOST,
+  port: process.env.REDIS_CLOUD_PORT ? "" : process.env.REDIS_PORT
 };
