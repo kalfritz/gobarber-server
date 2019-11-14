6175d4f2794ea5c03e2060f3b71be482a8c31836
Luiz Elias <luizeliasceem@gmail.com>
2019-11-14 00:42:29 -0300

    change the Procfile and package.json files

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require("dotenv/config");
 module.exports = {
   dialect: "postgres",
   host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
