ddc49a109092016c117ae18a03397748624b5cae
Luiz Elias <luizeliasceem@gmail.com>
2019-11-14 10:55:17 -0300

    logic fix on redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,5 @@
 export default {
-  host: process.env.REDIS_CLOUD_HOST ? "" : process.env.REDIS_HOST,
-  port: process.env.REDIS_CLOUD_PORT ? "" : process.env.REDIS_PORT,
+  host: process.env.REDIS_CLOUD_HOST || process.env.REDIS_HOST,
+  port: process.env.REDIS_CLOUD_PORT || process.env.REDIS_PORT,
   password: process.env.REDIS_CLOUD_PASS
 };
