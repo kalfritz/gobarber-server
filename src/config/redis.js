17ec98acaff85c70a119610d081260375651bd4c
Luiz Elias <luizeliasceem@gmail.com>
2019-11-14 11:08:10 -0300

    add auth_pass redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,6 @@
 export default {
   host: process.env.REDIS_CLOUD_HOST || process.env.REDIS_HOST,
   port: process.env.REDIS_CLOUD_PORT || process.env.REDIS_PORT,
-  password: process.env.REDIS_CLOUD_PASS
+  password: process.env.REDIS_CLOUD_PASS,
+  auth_pass: process.env.REDIS_CLOUD_PASS
 };
